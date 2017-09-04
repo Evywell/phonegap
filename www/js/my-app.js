e0dc555ad5fcc6014b64291033e91f1159bcdf8f
Evywell <leduc.axel.pro@gmail.com>
2017-09-04 12:06:17 +0200

    First commit

---
+++ www/js/my-app.js
@@ -0,0 +1,61 @@
+// Initialize app
+var myApp = new Framework7();
+
+// Ajax
+/**
+var request = new XMLHttpRequest();
+if (!request) {
+    console.error("Une erreur est survenu lors de l'initialisation de l'Ajax");
+}
+
+request.onreadystatechange = function () {
+    console.log(request);
+    if (request.readyState == XMLHttpRequest.DONE) {
+        if (request.status == 200) {
+            console.log(request.responseText);
+        } else {
+            console.error("Wrong request !");
+        }
+    }
+}
+request.open('GET', "https://jsonplaceholder.typicode.com/posts/1", true);
+request.send(null);
+**/
+// If we need to use custom DOM library, let's save it to $$ variable:
+var $$ = Dom7;
+
+// Add view
+var mainView = myApp.addView('.view-main', {
+    // Because we want to use dynamic navbar, we need to enable it for this view:
+    dynamicNavbar: true
+});
+
+// Handle Cordova Device Ready Event
+$$(document).on('deviceready', function() {
+    console.log("Device is ready!");
+});
+
+
+// Now we need to run the code that will be executed only for About page.
+
+// Option 1. Using page callback for page (for "about" page in this case) (recommended way):
+myApp.onPageInit('about', function (page) {
+    myApp.alert('ATRZATYZUYATZ');
+})
+
+// Option 2. Using one 'pageInit' event handler for all pages:
+$$(document).on('pageInit', function (e) {
+    // Get page data from event data
+    var page = e.detail.page;
+
+    if (page.name === 'about') {
+        // Following code will be executed for page with data-page attribute equal to "about"
+        myApp.alert('Here comes About page');
+    }
+})
+
+// Option 2. Using live 'pageInit' event handlers for each page
+$$(document).on('pageInit', '.page[data-page="about"]', function (e) {
+    // Following code will be executed for page with data-page attribute equal to "about"
+    myApp.alert('Here comes About page');
+})
